03e43e6adcf84dd5bcd625eab38fe45e709d97df
vivatakethat <vivatakethat@foxmail.com>
2019-07-15 03:02:03 +0800

    update 20170715_03:01am

--- json/to_string_literal.cpp
+++ json/to_string_literal.cpp
@@ -1,4 +1,4 @@
-#include </home/takethat/github_repository/json/single_include/nlohmann/json.hpp>
+#include <json.hpp>
 #include<iostream>
 using  namespace std;
 
@@ -13,29 +13,57 @@ json jj = "{ \"happy\": true, \"pi\": 3.141 }";
 string jjj="{ \"happy\": true, \"pi\": 3.141 }";
 
 // or even nicer with a raw string literal
-auto j2 = R"(
-  {
-    "happy": true,
-    "pi": 3.141
-  }
-)"_json;
-
+json j2 = {
+  {"pi", 3.141},
+  {"happy", true},
+  {"name", "Niels"},
+  {"nothing", nullptr},
+  {"answer", {
+    {"everything", 42}
+  }},
+  {"list", {1, 0, 2}},
+  {"object", {
+    {"currency", "USD"},
+    {"value", 42.99}
+  }}
+};
 auto j3=json::parse("{\"happy\":true,\"pi\":3.14}");
 //cout<<"[j]:\n"<<j<<endl;
 //cout<<"[jj]:\n"<<jj<<endl;
 //cout<<"[j2]\n"<<j2<<endl;
 //string s= j.dump();
 //string ss= j.dump(4);
-/*
-cout<<j<<endl;
-cout<<jj<<endl;
+
+//cout<<j2<<endl;
+
+
+//cout<<jj<<endl;
+/* 
 cout<<jjj<<endl;
 cout<<j2<<endl;
 cout<<j3<<endl;
 */
 
-string s=j.dump(4);
-cout<<s<<endl;
+
+/*-------------------------------------- */
+// a way to express the empty array []
+json empty_array_explicit = json::array();
+
+// ways to express the empty object {}
+json empty_object_implicit = json({});
+json empty_object_explicit = json::object();
+
+// a way to express an _array_ of key/value pairs [["currency", "USD"], ["value", 42.99]]
+json array_not_object = json::array({ {"currency", "USD"}, {"value", 42.99} });
+
+//cout<<empty_array_explicit<<endl;
+
+
+json arr_j;
+arr_j["list"] = { 1, 0, 2 };
+
+
+cout<<arr_j["list"]<<endl;
   return 0;
 
 }
